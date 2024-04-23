716a38533943ad0f7a640ecebfb0562a5c4873f1
kannyiri Safana <68133866+phantom-kas@users.noreply.github.com>
2024-04-23 16:31:35 +0800

    Create env.php

---
+++ env.php
@@ -0,0 +1,12 @@
+<?php
+define('DB_NAME','');
+ define('DEV_DB_USER','root');
+ define('DEV_DB_PASS','');
+
+define('DB_USER','');
+define('DB_PASS','');
+
+define('DEV',true);
+
+define('DB_HOST','localhost');
+<?php
